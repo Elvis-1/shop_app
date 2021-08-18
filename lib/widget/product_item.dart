7ffca5b764ab2aa8ab32209da03a8f799d5fe3b9
Elvis <66243414+Elvis-1@users.noreply.github.com>
2021-08-19 11:28:19 +1300

    started flutter shop app

---
+++ lib/widget/product_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  //const ProductItem({Key? key}) : super(key: key);
+  final String id;
+  final String title;
+  final String imageUrl;
+  
+  ProductItem(this.id, this.title, this.imageUrl);
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(child:Image.network(imageUrl, fit: BoxFit.cover,),
+      footer: GridTileBar(
+        leading:IconButton(
+          onPressed: (){},
+          icon: Icon(Icons.favorite,
+              color: Theme.of(context).accentColor
+          ),
+        ),
+        backgroundColor: Colors.black87,
+        title: Text(title, textAlign: TextAlign.center,),
+        trailing: IconButton(icon: Icon(Icons.shopping_cart,
+        color: Theme.of(context).accentColor,),
+        onPressed: (){},),
+      ),
+      ),
+    );
+  }
+}
